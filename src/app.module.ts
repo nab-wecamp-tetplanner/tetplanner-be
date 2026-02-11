93e7a3d93aa701b10362b0e18e54166d69eb5240
Khong <hankhongg@gmail.com>
2026-02-12 00:40:46 +0700

    chore(db_schem): add missin entity and reconfig eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { TimelinePhasesModule } from './timeline_phases/timeline_phases.module';
 import { TodoItemsModule } from './todo_items/todo_items.module';
 import { BudgetTransactionsModule } from './budget_transactions/budget_transactions.module';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [UsersModule, TetConfigsModule, CategoriesModule, TimelinePhasesModule, TodoItemsModule, BudgetTransactionsModule, CollaboratorsModule],
+  imports: [UsersModule, TetConfigsModule, CategoriesModule, TimelinePhasesModule, TodoItemsModule, BudgetTransactionsModule, CollaboratorsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
