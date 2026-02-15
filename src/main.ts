5068d8f38d53e08bb5adb56f4e406c8661b89f40
Khong <hankhongg@gmail.com>
2026-02-15 17:38:26 +0700

    feat(setup): setup swagger + cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './interceptors/response.interceptors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,30 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000'], // web
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    //preflightContinue: false,
+    //optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('TetPlanner API')
+    .setDescription('The TetPlanner API description')
+    .setVersion('1.0')
+    .addTag('tetplanner')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authenticate',
+      description: 'Enter JWT token',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
